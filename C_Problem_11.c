f8921b8b34818dd3ba9df0a71ab4c3d720f60c3f
Niranjana <ponniranjana2006@gmail.com>
2026-06-03 11:17:26 +0530

    Create C_Problem_11.c

---
+++ C_Problem_11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+   int a,ones,tens,result;
+   printf("Enter a two digit number:");
+   scanf("%d",&a);
+   ones=a%10;
+   tens=a/10;
+   result=(tens<=ones);
+   printf("%d",result);
+   return 0;
+}
